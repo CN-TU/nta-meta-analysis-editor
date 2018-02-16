e2fcb2c442904b45c34ee356723aafceb741fd3b
Gernot Vormayr <gernot.vormayr@nt.tuwien.ac.at>
2018-02-16 11:48:50 +0100

    Better electron integration

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 var {app, BrowserWindow} = require('electron');
 
-app.on('ready', function () {
-    var mainWindow = new BrowserWindow({
+function openFeatureEditor(feature, context, base_path) {
+    var featureWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegrationInWorker: true
         }
     });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.openDevTools();
+    featureWindow.ntarc_feature = feature;
+    featureWindow.ntarc_context = context;
+    featureWindow.ntarc_base_path = base_path;
+    featureWindow.loadURL('file://' + __dirname + '/index.html');
+    featureWindow.openDevTools();
+}
+
+app.on('ready', function () {
+    openFeatureEditor(
+        '[{"map": [{"multiply": [{"add": [{"add": ["transportOctetDeltaCount",40]},1000]},{"subtract": [{"multiply": ["flowDirection",2]},1]}]},{"select": [{"greater": ["transportOctetDeltaCount",0]}]}]}]',
+        "flows",
+        ".")
 });
 
 app.on('window-all-closed', function () {
