f565ee24ab1fe41305faa098d5ed99301fca7196
Gernot Vormayr <gernot.vormayr@nt.tuwien.ac.at>
2018-04-10 17:57:23 +0200

    Make entry safer (prevent arbitrary urls), reformatting

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-const {app, BrowserWindow, ipcMain, dialog, net, webContents} = require('electron')
+const { app, BrowserWindow, ipcMain, dialog, net, webContents } = require('electron')
 const path = require('path')
 const url = require('url')
 const https = require('https')
 const fs = require('fs')
 const download = require('download-tarball');
 
-const {PROJECT, API_URL, needed} = require('./config.js');
+const { PROJECT, API_URL, needed } = require('./config.js');
 
 var windows = {}
 var helpwindow = null
 const base_path = path.join(app.getPath('appData'), 'ntarc');
+
+let INDEX_URL = url.format({
+    pathname: path.join(__dirname, "build/index.html"),
+    protocol: 'file:',
+    slashes: true
+})
+if (global.DEBUG === true && process.env.ELECTRON_START_URL !== undefined)
+    INDEX_URL = process.env.ELECTRON_START_URL;
 
 function displayHelp(url) {
     if (helpwindow === null) {
@@ -21,10 +29,10 @@ function displayHelp(url) {
 
             }
         });
-        helpwindow.on('closed', ()=>{
+        helpwindow.on('closed', () => {
             helpwindow = null;
         })
-        helpwindow.on('page-title-updated', (event)=>{
+        helpwindow.on('page-title-updated', (event) => {
             event.preventDefault();
         })
         helpwindow.setMenu(null);
@@ -43,14 +51,12 @@ function createWindow (filename) {
     windows[id] = win
 
     win.ntarc_filename = filename;
-    win.loadURL(process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, "build/index.html"),
-        protocol: 'file:',
-        slashes: true
-    }))
+    win.loadURL(INDEX_URL);
 
     win.webContents.on('will-navigate', (event, url) => {
-        if ((url.startsWith('http://') || url.startsWith('https://')) && !url.startsWith(process.env.ELECTRON_START_URL)) {
+        if ((url.startsWith('http://') || url.startsWith('https://'))) {
+            if (global.DEBUG === true && url.startsWith(process.env.ELECTRON_START_URL))
+                return;
             event.preventDefault()
             displayHelp(url)
         }
@@ -84,20 +90,20 @@ function copy(src, dst) {
 
 function updateAvailable(sha, tag) {
     result = dialog.showMessageBox({
-            "type": "info",
-            "title": "Update available",
-            "message": "There is a newer specification available. Update?",
-            "buttons": ["Yes", "No"],
-            "defaultId": 0,
-            "cancelId": 1
+        "type": "info",
+        "title": "Update available",
+        "message": "There is a newer specification available. Update?",
+        "buttons": ["Yes", "No"],
+        "defaultId": 0,
+        "cancelId": 1
     })
     if (result == 0) {
         download({
-            url: API_URL + PROJECT + '/tarball/'+tag,
+            url: API_URL + PROJECT + '/tarball/' + tag,
             dir: base_path,
             extractOpts: {
                 ignore: (_, header) => {
-                    if(needed.has(header.name)) return false;
+                    if (needed.has(header.name)) return false;
                     return true;
                 },
                 map: header => {
@@ -106,12 +112,12 @@ function updateAvailable(sha, tag) {
                 }
             }
         }).then(() => {
-            fs.writeFileSync(path.join(base_path, 'commit.json'), JSON.stringify({sha:sha, tag:tag}));
+            fs.writeFileSync(path.join(base_path, 'commit.json'), JSON.stringify({ sha: sha, tag: tag }));
             dialog.showMessageBox({
-                    "type": "info",
-                    "title": "Update downloaded",
-                    "message": "Update successfully downloaded. To use the new specification, reopen all windows.",
-                    "buttons": ["Ok"]
+                "type": "info",
+                "title": "Update downloaded",
+                "message": "Update successfully downloaded. To use the new specification, reopen all windows.",
+                "buttons": ["Ok"]
             });
             console.log("update ok!")
         }).catch(err => {
@@ -127,7 +133,7 @@ function updateAvailable(sha, tag) {
 
 function checkUpdate() {
     let link = url.parse(API_URL + PROJECT + "/tags");
-    link.headers =  {'User-Agent': 'CN-TU/nta-meta-analysis-editor updater'};
+    link.headers = { 'User-Agent': 'CN-TU/nta-meta-analysis-editor updater' };
     req = net.request(link);
     req.on('response', (response) => {
         if (response.statusCode == 200) {
@@ -137,10 +143,10 @@ function checkUpdate() {
             })
             response.on('end', () => {
                 data = JSON.parse(data)
-                data = new Map(data.map(tag => {return [tag.name, tag.commit.sha];}));
+                data = new Map(data.map(tag => { return [tag.name, tag.commit.sha]; }));
                 let tags = Array.from(data.keys()).sort();
                 let latestTag = tags[tags.length - 1];
-                let {sha, tag} = JSON.parse(fs.readFileSync(path.join(base_path, 'commit.json'), 'utf8'));
+                let { sha, tag } = JSON.parse(fs.readFileSync(path.join(base_path, 'commit.json'), 'utf8'));
                 if (latestTag > tag) {
                     updateAvailable(data.get(latestTag), latestTag);
                 } else {
@@ -157,21 +163,20 @@ function checkUpdate() {
 
 app.on('ready', () => {
     var needFix = false;
-    for(let f of needed) {
-        if(!fs.existsSync(path.join(base_path, f)))
-        {
+    for (let f of needed) {
+        if (!fs.existsSync(path.join(base_path, f))) {
             needFix = true;
             break;
         }
     }
-    if(needFix == false) {
-        if(!fs.existsSync(path.join(base_path, 'commit.json'))) {
+    if (needFix == false) {
+        if (!fs.existsSync(path.join(base_path, 'commit.json'))) {
             needFix = true;
         }
     }
     if (needFix) {
         fs.mkdirSync(base_path);
-        for(let f of needed)
+        for (let f of needed)
             copy(path.join(__dirname, 'spec', f), path.join(base_path, f))
         copy(path.join(__dirname, 'spec', 'commit.json'), path.join(base_path, 'commit.json'))
     }
